59fba84574ac2357544e888da7864fd1ec9ab50e
danh0854187113 <iamfroggy62@gmail.com>
2025-11-23 11:59:23 +0700

    first commit

--- buoi7.py
+++ buoi7.py
@@ -1,40 +1,52 @@
-import random  #KHAI BÁO RANDOM 
-p = input("nhập số dòng: ") # Tạo biến p để nhập số dòng
-while p.isdigit()== False: # Nếu P không phải là định dạng số
-    p = (input("nhập số:  ")) # bắt nhập lại
-p = int(p)# p phải là số thực
-a=(input("nhập độ dài của mật khẩu: "))#tạo biến a để nhập độ dài mk
-while a.isdigit()== False:# Nếu a không phải là định dạng số
-    a = (input( "nhập số đi bố: "))#bắt nhập lại
-a=int(a)# a phải là số thực
-require_all= input("Nhap cac dieu kien: ")# nhập điều kiện true or false
-while a <= 0:# nếu a <= 0
- a= int (input("nhập lại độ dài nhiều hơn 0 kí tự: "))#bắt nhập lại
-if require_all == "true": #nếu tất cả điều kiện đúng 
-    while a < 4:#khi a <4
-        a=int(input("phải lớn hơn 4: "))#bắt nhập lại
-        print("độ dài mật khẩu là:",a)#in độ dài mk ra màn hình
-letters = ("qwertyuiopasdfghjklzxcvbnm")#các chữ để random
-numbers = ("1234567890")#các số để random
-symbols = ("!@#$%^&*")#các kí tự để random
-password = ""#biến mk
-if require_all == "true":#khi tất cả dk = true
-    for i in range(p):#biến i
-        for i in range(a):#tạo vòng lặp
-            if i == 0:#khi i bằng 0
-                password+=random.choice(letters.upper())#vị trí 0 sẽ là chữ hoa
-            elif i == 1:#khi i bằng 1
-                password+=random.choice(symbols)# vị trí 1 sẽ là kí tự
-            elif i == 2:#khi i bằng 2
-                password+=random.choice(letters)#vị trí 2 là chữ
-            elif i == 3:#khi i bằng 3
-                password+=random.choice(numbers)#vị trí 3 là số
+import random #tạo biến random
+#Nhập số dòng
+p = input("nhập số dòng: ")
+#Kiểm tra định dạng đầu vào
+while p.isdigit()== False:
+    p = (input("nhập số:  "))
+p = int(p)
+# Nhạp độ dài
+a=(input("nhập độ dài của mật khẩu: "))
+# Kiểm tra định dạng đầu vào
+while a.isdigit()== False:
+    a = (input( "nhập số đi bố: "))
+a=int(a)
+#kiểm tra định dạng đầu vào
+require_all= input("Nhap cac dieu kien: ")
+while a <= 0:
+ a= int (input("nhập lại độ dài nhiều hơn 0 kí tự: "))
+if require_all == "true":
+    while a < 4:
+        a=int(input("phải lớn hơn 4: "))
+        #tạo mật khẩu
+        print("độ dài mật khẩu là:",a)
+# tạo cacs biến mật khẩu
+letters = ("qwertyuiopasdfghjklzxcvbnm")# chữ
+numbers = ("1234567890")#số
+symbols = ("!@#$%^&*")#kí tự
+
+# TẠO MẬT KHẨU
+password = ""
+#kiểm tra định dạng đầu vào
+# Tạo mật khẩu theo điều kiện
+if require_all == "true":
+    for i in range(p):
+        for i in range(a):
+            if i == 0:
+                password+=random.choice(letters.upper())
+            elif i == 1:
+                password+=random.choice(symbols)
+            elif i == 2:
+                password+=random.choice(letters)
+            elif i == 3:
+                password+=random.choice(numbers)
             else:
                 password+= random.choice(letters + numbers + symbols + letters.upper())
-        password += ' \n'#công thức mk
+        password += ' \n'
+# Tạo mật khẩu tự do
 else:
-    for i in range(p):#vòng lập i
-        for i in range(a):#vòng lăpj
+    for i in range(p):
+        for i in range(a):
           password += random.choice( letters + numbers + symbols + letters.upper())
-        password+="\n"#tạo ra mk
-print("Mật khẩu của bạn là: \n",password)#in ra mk
+        password+="\n"
+print(password)
